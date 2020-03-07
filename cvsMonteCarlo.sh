824bdcba3ab490d736fad832efd0a31b7243d6e4
Rafael Sequeira <rm.sequeira@campus.fct.unl.pt>
2020-03-07 21:03:09 +0000

    Small typo fixed

--- cvsMonteCarlo.sh
+++ cvsMonteCarlo.sh
@@ -4,13 +4,13 @@ TIMES=(1000 10000 100000 100000 1000000 10000000 100000000)
 
 if (($# < 4))
 then
-	echo $0 output_file serial_estimator paralel_estimator num_iterations
+	echo $0 output_file serial_estimator parallel_estimator num_iterations
 	exit 1
 fi
 
 output_file=$1
 serial_estimator=$2
-paralel_estimator=$3
+parallel_estimator=$3
 num_iterations=$4
 
 printf "Serial " >> $output_file
@@ -19,7 +19,7 @@ do
 	printf " " >> $output_file
 done
 
-printf "Paralel" >> $output_file
+printf "Parallel" >> $output_file
 printf "\n" >> $output_file
 
 for i in ${TIMES[@]}
@@ -43,7 +43,7 @@ do
 
 	for t in ${TIMES[@]}
 	do
-		iteration_time=$({ time java $paralel_estimator $t >/dev/null;} 2>&1 | grep real | tail -c 7 | head -c 5)
+		iteration_time=$({ time java $parallel_estimator $t >/dev/null;} 2>&1 | grep real | tail -c 7 | head -c 5)
 		printf "$iteration_time " >> $output_file
 	done
 
